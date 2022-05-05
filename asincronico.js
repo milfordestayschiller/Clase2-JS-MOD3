e768540b17b80dcaaf19d21f8c292c05964624f8
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-05-05 16:34:45 -0400

    Clase Asincronica
    Clase Asincronica

---
+++ asincronico.js
@@ -0,0 +1,11 @@
+var dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado',
+]
+alert("El dia de la semana es " + dias[new Date().getDay()]);
+var ingresaNumeroDia = prompt("Ingresa el valor numerico del dia entendiendo que el dia domingo es 0 hasta el sabado dia 6")
+
+alert("El numero ingresado " +ingresaNumeroDia + " " + "corresponde a" + " " +dias[ingresaNumeroDia])
+
+
+
+
+
